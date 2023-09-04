1abb740da5b3d673c29174c8a153bb83e9c44970
ivant0110 <ivantokic0110@gmail.com>
2023-09-04 02:23:23 +0200

    added cart modal

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import React from 'react';
 
 import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,10 +1,10 @@
 import { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-
+import React from 'react';
 import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} onClick={props.onClose}/>;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
